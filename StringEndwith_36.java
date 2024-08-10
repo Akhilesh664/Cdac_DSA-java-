5e1a09291356a15fe0d27c8f00fd8969573305b5
Akhilesh <akhil00664@gmail.com>
2024-08-10 20:16:04 +0530

    new file uploaded

---
+++ StringEndwith_36.java
@@ -0,0 +1,11 @@
+
+public class StringEndwith_36 {
+	
+	public static void main(String[] args) 
+	{
+		String str = "java developer at regex";
+		System.out.println(str.endsWith("x"));
+		System.out.println(str.endsWith("regex"));
+		System.out.println(str.endsWith("a"));		
+	}
+}
