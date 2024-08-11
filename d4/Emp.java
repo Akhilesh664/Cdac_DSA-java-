9624ea4b87e2412a6b80f05623baf32aa8f1f4c6
Akhilesh <akhil00664@gmail.com>
2024-08-11 23:18:25 +0530

    updates

--- d4/Emp.java
+++ d4/Emp.java
@@ -11,6 +11,8 @@ public class Emp {
         this.sal = sal;
     }
     
+
+    
     public void dispEmp() {
         System.out.println("Employee Number: " + eno + ", Name: " + ename + ", Salary: " + sal);
     }
